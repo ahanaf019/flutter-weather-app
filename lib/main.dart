a01b1ec14ae317b80500863310c2ce1b41c0f8e1
Md. Ahanaf Arif Khan <ahanaf019@gmail.com>
2022-09-27 20:49:21 +0600

    Weather app complete.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/weather.dart';
 
 
 void main(List<String> args) {
@@ -6,21 +7,67 @@ void main(List<String> args) {
 }
 
 
-class WeatherApp extends StatelessWidget {
+class WeatherApp extends StatefulWidget {
+  const WeatherApp({super.key});
+  
+  @override
+  State<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  late Future<Weather> futureWeather;
+
+  @override
+  void initState() {
+    super.initState();
+    futureWeather = getWeather(24.37, 88.60);
+    print(futureWeather);
+  }
 
   @override
   Widget build(BuildContext context) {
-
- 
     return MaterialApp(
       title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Weather App'),
+          title: const Text('Weather App'),
         ),
-        body: Center(child: Text('Text')),
+        body: Center(
+          child: FutureBuilder<Weather>(
+            future: futureWeather,
+            builder: (context, snapshot) {
+              print(snapshot.hasData);
+              if (snapshot.hasData) {
+
+                var stringText = '''
+          Location: 
+            Rajshahi.
+            Lat=${snapshot.data!.lat}, Lon=${snapshot.data!.lon}
+          
+          Weather:
+                Temperature     : ${snapshot.data!.temp} °C
+                Feels Like          : ${snapshot.data!.feelsLike} °C
+                Min                     : ${snapshot.data!.tempMin} °C
+                Max                    : ${snapshot.data!.tempMax} °C
+                Humidity            : ${snapshot.data!.humidity}%
+                Pressure            : ${snapshot.data!.pressure} hPa
+                Wind Speed       : ${snapshot.data!.windSpeed} meter/sec
+                ''';
+                
+                return Text(stringText);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
+      ),
     );
   }
-
 }
--- lib/weather.dart
+++ lib/weather.dart
@@ -2,26 +2,71 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 import 'dart:convert';
 
-Future<Map<dynamic, dynamic>> getWeather(double lat, double lon) async {
-  var key = await File('resources/key.txt').readAsLines();
+
+Future<Weather> getWeather(double lat, double lon) async {
   
   Map<String, String> queryParams = {
     'lat': lat.toString(),
     'lon': lon.toString(),
-    'appid': key[0]
+    'appid': 'da9bd6142b364f2df393f0487cd9193d',
+    'units': 'metric'
   };
 
   Uri requestUrl = Uri.https('api.openweathermap.org', '/data/2.5/forecast', queryParams);
   var response = await http.get(requestUrl);
 
 
-  Map responsData = jsonDecode(response.body);
-  return responsData;
+  Map<String, dynamic> responsData = jsonDecode(response.body);
+  // responsData = responsData['list'][0]['main'];
+
+  return Weather.fromJson(responsData);
+}
+
+class Weather {
+  final String lat;
+  final String lon;
+  final String windSpeed;
+  final String temp;
+  final String feelsLike;
+  final String tempMin;
+  final String tempMax;
+  final String pressure;
+  final String humidity;
+
+  const Weather({
+    required this.lat,
+    required this.lon,
+    required this.windSpeed,
+    required this.temp,
+    required this.feelsLike,
+    required this.tempMin,
+    required this.tempMax,
+    required this.humidity,
+    required this.pressure,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      lat : json['city']['coord']['lat'].toString(),
+      lon : json['city']['coord']['lon'].toString(),
+      windSpeed : json['list'][0]['wind']['speed'].toString(),
+      temp: json['list'][0]['main']['temp'].toString().toString(),
+      feelsLike: json['list'][0]['main']['feels_like'].toString(),
+      tempMin: json['list'][0]['main']['temp_min'].toString(),
+      tempMax: json['list'][0]['main']['temp_max'].toString(),
+      humidity: json['list'][0]['main']['humidity'].toString(),
+      pressure: json['list'][0]['main']['pressure'].toString(),
+
+    );
+  }
 }
 
 
-void main(List<String> args) async{
-    var weather = await getWeather(22.2, 44.2);
-    print(weather);
+// void main(List<String> args) async{
+//     var weather = await getWeather(22.2, 44.2);
+//     print(weather.temp);
+//     print(weather.windSpeed);
 
-}
+//     // print(weather); 
+
+// }
